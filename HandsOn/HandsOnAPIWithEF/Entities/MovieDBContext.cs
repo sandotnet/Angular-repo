7909f051885fae2c3373aaa022140907060bc6e3
sandotnet <87011674+sandotnet@users.noreply.github.com>
2023-12-12 14:53:47 +0530

    commit

--- HandsOn/HandsOnAPIWithEF/Entities/MovieDBContext.cs
+++ HandsOn/HandsOnAPIWithEF/Entities/MovieDBContext.cs
@@ -10,7 +10,8 @@ namespace HandsOnAPIWithEF.Entities
         public DbSet<Movie> Movies { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"server=DESKTOP-4O1D65I\SQLEXPRESS;database=Movie518DB;trusted_connection=true");
+             optionsBuilder.UseSqlServer(@"server=DESKTOP-4O1D65I\SQLEXPRESS;database=Movie518DB;trusted_connection=true");
+            //optionsBuilder.UseSqlServer(@"Server=tcp:myserver12.database.windows.net,1433;Initial Catalog=moviedb;Persist Security Info=False;User ID=myserver;Password=server123@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
     }
 }
